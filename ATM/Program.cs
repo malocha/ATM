40124d8a95a3fbac89bb50ae1cc8f8dc031c298e
Weronika <Weronika@DESKTOP-AU8FPML>
2023-02-22 22:58:46 +0100

    Dodaj pliki projektów.

---
+++ ATM/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace ATM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            public class cardHolder
+        {
+            String cardNum;
+            int pin;
+            String firstName;
+            String lastName;
+            double balance;
+
+            public cardHolder(String cardNum, int pin, String firstName, String lastName, double balance)
+            {
+                this.cardNum = cardNum;
+                this.pin = pin;
+                this.firstName = firstName;
+                this.lastName = lastName;
+                this.balance = balance;
+            }
+
+            public.String getNum() 
+            { 
+                return cardNum;
+            } 
+            public int getPin() 
+            { 
+                return pin;
+            }
+
+            public String getFirstName()
+            {
+                return firstName;
+            }
+
+            public String getLastName()
+            {
+                return lastName;   
+            }
+
+            public double getBalance()
+            {
+                return balance;
+            }
+
+            public void setNum(String newCardNum)
+            {
+                cardNum = newCardNum;
+            }
+
+            public void setPin(int newPin)
+            { pin = newPin; }
+
+            public void setFirstName(String newFirstName) 
+            { 
+                firstName = newFirstName; 
+            }
+            public void setLastName(String newLastName) 
+            { 
+                lastName = newLastName; 
+            } 
+
+            public void setBalance(double newBalance)
+            {
+                balance = newBalance;   
+            }
+
+            public static void Main(String[] args)
+            {
+                void printOptions()
+                {
+                    Console.WriteLine("Please choose from one of the following options...");
+                    Console.WriteLine("1. Deposit");
+                    Console.WriteLine("2. Withdraw");
+                    Console.WriteLine("3. Show Balance");
+                    Console.WriteLine("4. Exit");
+                }
+
+                void deposit(cardHolder currentUser)
+                {
+
+                }
+            }
+        }
+
+        }
+    }
+}
